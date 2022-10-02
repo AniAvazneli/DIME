e3e4c0ba83aeb940125c787d2eb3fe64206f3949
AniAvazneli <avazneli.ani@gmail.com>
2022-10-02 12:34:11 +0400

    first commit

--- hydrogen.config.js
+++ hydrogen.config.js
@@ -6,11 +6,11 @@ export default defineConfig({
     defaultLanguageCode: 'EN',
     storeDomain:
       // @ts-ignore
-      Oxygen?.env?.PUBLIC_STORE_DOMAIN || 'hydrogen-preview.myshopify.com',
+      Oxygen?.env?.PUBLIC_STORE_DOMAIN || 'dime-beauty-demo.myshopify.com',
     storefrontToken:
       // @ts-ignore
       Oxygen?.env?.PUBLIC_STOREFRONT_API_TOKEN ||
-      '3b580e70970c4528da70c98e097c2fa0',
+      'df6fc859931d69c4b8a6d2fbb6ea2286',
     privateStorefrontToken:
       // @ts-ignore
       Oxygen?.env?.PRIVATE_STOREFRONT_API_TOKEN,
